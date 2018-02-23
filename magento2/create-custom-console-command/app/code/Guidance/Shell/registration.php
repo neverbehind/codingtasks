9cc2d62033ba8cf2f93a7198ff6fd317c4d202bd
Andrew Hofstetter <ahofs@guidance.com>
2018-02-23 14:57:09 -0800

    Add files for last order command line script

---
+++ magento2/create-custom-console-command/app/code/Guidance/Shell/registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Guidance Shell
+ *
+ * @author      Guidance Magento Team <magento@guidance.com>
+ * @category    Guidance
+ * @package     Shell
+ * @copyright   Copyright (c) 2018 Guidance Solutions (http://www.guidance.com)
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Guidance_Shell',
+    __DIR__
+);
